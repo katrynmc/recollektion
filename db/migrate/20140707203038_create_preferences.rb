6213c74e2be26b83a5a3ca1a207f01b2b56a21bc
testkitschen <katryn.mcintosh@gmail.com>
2014-07-07 18:47:14 -0400

    Create migration for preferences, migrate and rake routes

--- db/migrate/20140707203038_create_preferences.rb
+++ db/migrate/20140707203038_create_preferences.rb
@@ -1,6 +1,14 @@
 class CreatePreferences < ActiveRecord::Migration
   def change
     create_table :preferences do |t|
+      t.references :category
+      t.references :dependent
+      t.text :brand
+      t.text :product_name
+      t.integer :score, index: true
+      t.text :comment
+
+      t.timestamps
     end
   end
 end
