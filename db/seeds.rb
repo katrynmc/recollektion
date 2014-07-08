ca16e15764fd2419efd49f98e9ca7e272f2c43a8
testkitschen <katryn.mcintosh@gmail.com>
2014-07-08 09:32:11 -0400

    Create basic seed data for app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+categories = Category.create([{ name: 'books' }, { name: 'dog food' }, { name: 'eating with friends' }, { name: 'medications' }, { name: 'movies' }, { name: 'video games' }, { name: 'makeup' }, { name: 'dog toys' }])
+
+Preference.create(brand: 'wellness', product_name: 'CORE Ocean Formula Adult Dog Food', score: 4, comment: 'Smells kinda fishy, but he seems to tolerate it well')
+
+Preference.create(brand: 'Coca-Cola', product_name: 'Diet Coke', score: 5, comment: 'Delicious')
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
--- spec/support/session_helpers.rb
+++ spec/support/session_helpers.rb
@@ -7,3 +7,4 @@ module SessionHelpers
     click_button 'Sign in'
   end
 end
+
