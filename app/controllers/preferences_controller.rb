590e3e339518be6382313e698d55eb9455022488
testkitschen <katryn.mcintosh@gmail.com>
2014-07-09 13:23:32 -0400

    Add limit to index

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,5 +1,5 @@
 class PreferencesController < ApplicationController
   def index
-     @preferences = Preference.order(score: :desc)
+     @preferences = Preference.order(score: :desc).limit(18)
   end
 end
