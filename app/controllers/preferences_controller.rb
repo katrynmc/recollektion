adc9890d248242786b0628a724e0cb1e2b7e47f4
testkitschen <katryn.mcintosh@gmail.com>
2014-07-09 13:45:40 -0400

    Create new definition in controller and new view for preferences

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,5 +1,21 @@
 class PreferencesController < ApplicationController
+
   def index
      @preferences = Preference.order(score: :desc).limit(18)
   end
+
+  def show
+    @preference = Preference.find(params[:id])
+  end
+
+  def new
+    @preference = Preference.new
+  end
+
+  private
+
+  def preference_params
+    params.require(:preference).permit([:brand, :product_name, :score, :comment])
+  end
+
 end
