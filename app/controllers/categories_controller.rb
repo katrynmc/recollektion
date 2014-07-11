9f27f43ddc7a9a58b33d7522193d89a88e3f6534
testkitschen <katryn.mcintosh@gmail.com>
2014-07-11 18:28:48 -0400

    User can successfully create a category and is then redirected to the home page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,15 +4,24 @@ class CategoriesController < ApplicationController
      @categories = Category.order(name: :desc)
   end
 
-   def new
-
+  def new
     @category = Category.new
+  end
 
+  def create
+
+    @category = current_user.categories.new( category_params )
+
+    if @category.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def category_params
-      params.require(:name).permit(:name)
+      params.require(:category).permit([:name])
   end
 end
