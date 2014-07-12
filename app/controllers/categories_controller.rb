daaa54d89f43e0a51463b8096134889f8da0e3be
testkitschen <katryn.mcintosh@gmail.com>
2014-07-11 23:30:10 -0400

    Add edit and delete buttons to categories index - edit still requires a view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,15 +13,44 @@ class CategoriesController < ApplicationController
     @category = current_user.categories.new( category_params )
 
     if @category.save
+      flash[:success] = 'New category created.'
       redirect_to root_path
     else
       render :new
     end
   end
 
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    @category = current_user.category.assign_attributes(category_params)
+
+    if @category.save
+      flash[:success] = 'Category updated.'
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy!
+    flash[:success] = 'Category deleted.'
+    redirect_to user_categories_path
+  end
+
   private
 
   def category_params
       params.require(:category).permit([:name])
+  end
+
+  def find_category
+    @category = Category.find(params[:id])
   end
 end
