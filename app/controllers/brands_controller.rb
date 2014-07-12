25e5698e6ab642bd1aaf83e20282e056027c7f2d
testkitschen <katryn.mcintosh@gmail.com>
2014-07-11 21:44:21 -0400

    Create controller for items add methods to create new items

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -18,6 +18,6 @@ class BrandsController < ApplicationController
   private
 
   def brand_params
-      params.require(:category).permit([:name])
+      params.require(:brand).permit([:name])
   end
 end
